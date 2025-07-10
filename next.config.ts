073966a2a5092753b2da1ae5584d297562f9cc1e
Anton S <antottoturial@gmail.com>
2025-07-10 16:49:22 +0000

    Initial commit of production site

--- next.config.ts
+++ next.config.ts
@@ -22,8 +22,14 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'http',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
-  },
+  },  
   allowedDevOrigins: [
     'https://6000-firebase-studio-1751707908116.cluster-c23mj7ubf5fxwq6nrbev4ugaxa.cloudworkstations.dev',
   ],
