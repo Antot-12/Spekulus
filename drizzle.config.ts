c29c7d02aa641b05b824e8c84d4c4eea429800dc
Anton S <antottoturial@gmail.com>
2025-07-11 20:45:57 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
   dbCredentials: {
     url: process.env.DATABASE_URL,
   },
-  tablesFilter: ["advantages", "dev_notes", "faq_items", "hero_sections", "product_components", "roadmap_events", "creators"],
+  tablesFilter: ["advantages", "dev_notes", "faq_items", "hero_sections", "product_components", "roadmap_events", "creators", "action_sections", "languages"],
   out: "./drizzle",
   verbose: true,
   strict: true,
